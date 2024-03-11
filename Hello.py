e99dc8b3152045358e95cf0a9c5f98c2df4af97c
raymondwck <151514247+raymondwck@users.noreply.github.com>
2024-03-11 16:05:47 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -7,7 +7,15 @@ def run():
     )
 
     st.write("Japanese Food Recommendation System")
-
+    # Define a list of values for the dropdown
+    options = ["California Roll", "Salmon Nigiri", "Tonkotsu Ramen", "Chicken Teriyaki Bento", "Edamame", "Gyoza (Dumplings)", "Tempura (Shrimp)", 
+            "Green Tea Ice Cream", "Mochi Ice Cream", "Matcha Latte"]
+    
+    # Create the dropdown list using selectbox
+    selected_option = st.selectbox('Select an option:', options)
+    
+    # Display the selected option
+    st.write('You selected:', selected_option)
 
 
 if __name__ == "__main__":
