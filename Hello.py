c213d130d595e3aed821efd084f95be31fdc6517
raymondwck <151514247+raymondwck@users.noreply.github.com>
2024-03-11 15:50:43 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,13 +1,17 @@
 import streamlit as st
 from streamlit.logger import get_logger
-
+import Assignment 1 (1)
 def run():
     st.set_page_config(
         page_title="Data Mining Individual Assignment 1 ",
         page_icon="👋",
     )
 
-    st.write("Data Mining Individual Assignment ")
+    st.write("Japanese Food Recommendation System")
+    # Using the function from the imported module
+    result = Assignment 1 (1).recommendFood()
+    
+    st.write("Result:", result)
 
 
 if __name__ == "__main__":
