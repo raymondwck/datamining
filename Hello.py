47a92a407792f276668c42d74c0bc8a1ad2216b6
raymondwck <151514247+raymondwck@users.noreply.github.com>
2024-03-11 15:40:32 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,9 +1,6 @@
 import streamlit as st
 from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
-
-
 def run():
     st.set_page_config(
         page_title="Data Mining Individual Assignment 1 ",
