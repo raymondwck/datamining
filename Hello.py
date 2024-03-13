7bf4da4db032e8c65f1e14fc92a74819be50a7e6
raymondwck <151514247+raymondwck@users.noreply.github.com>
2024-03-13 14:04:23 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -98,10 +98,8 @@ def main():
     if st.button("Recommend"):
         rules = recommendFood(initial_order, X, features)
         for rule in rules:
-            premise_index, conclusion_index = rule  # Extract premise and conclusion indices
-            premise_feature = features[premise_index]  # Get the feature name for the premise
-            conclusion_feature = features[conclusion_index]  # Get the feature name for the conclusion
-            st.write(f"Rule: If a person buys {premise_feature}, they will also buy {conclusion_feature}")
+            st.write(rule)
+            st.write(rules[rule])
             
 if __name__ == "__main__":
     main()
